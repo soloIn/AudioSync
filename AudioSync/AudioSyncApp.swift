7b8d53414ae15b7c469660789fa789fc42dfdeba
solo <1664931260@qq.com>
2025-04-29 13:14:40 +0800

    Initial Commit

---
+++ AudioSync/AudioSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioSyncApp.swift
+//  AudioSync
+//
+//  Created by solo on 4/29/25.
+//
+
+import SwiftUI
+
+@main
+struct AudioSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
